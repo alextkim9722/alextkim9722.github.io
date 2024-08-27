a2936bf886b18d15aa90d6f783d9b3d95f42b1b4
alextkim9722 <82482033+alextkim9722@users.noreply.github.com>
2024-08-26 19:51:24 -0700

    Blocking Out Designs
    - Brainstorming designs for the main page
    - Blocked out locations of features using CSS

---
+++ Portfolio/src/App.tsx
@@ -0,0 +1,21 @@
+import Sidebar from "./sidebar/Sidebar"
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div id="container">
+      <div id="header">
+      </div>
+      <div id="content">
+        <div id="profile"></div>
+        <div id="widget"></div>
+        <div id="description"></div>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
